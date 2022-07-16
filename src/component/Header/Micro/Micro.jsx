69de91cc4ed82b181d297b6bc569368a82ee6e2c
Asker <kotsevasker0@gmail.com>
2022-07-16 16:30:25 +0300

    main

--- src/component/Header/Micro/Micro.jsx
+++ src/component/Header/Micro/Micro.jsx
@@ -3,7 +3,9 @@ import Style from './micro.style.module.css';
 function Micro() {
   const [state,setState] = useState(true);
   return (
-    <div className={state?Style.micro:Style.active} onClick={()=>setState(prev => !prev)}>
+    <div className={state?Style.micro:Style.active} onClick={(e)=>{
+      e.stopPropagation();
+      setState(prev => !prev)}}>
       <i  class="fa-solid fa-microphone"></i>
     </div>
   )
